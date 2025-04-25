a650e86ac4ec8bf706497ee82f81f1b817e8d19e
dQqAn <doganyilmaz99@hotmail.com>
2025-04-25 22:30:35 +0300

    pagination

--- src/main/resources/static/js/audio_player/audio_player.js
+++ src/main/resources/static/js/audio_player/audio_player.js
@@ -196,13 +196,62 @@ function loadFavourites(userID, containerID, page = 1) {
             listItem.appendChild(controllerDiv)
             container.appendChild(listItem)
         });
+
+        const paginationDiv = document.createElement('div')
+        paginationDiv.id = "pagination"
+        paginationDiv.className = "pointer pagination"
+        container.appendChild(paginationDiv)
+
         lucide.createIcons();
         window.history.pushState({page: page}, `Page ${page}`, `?page=${page}`);
 
+        const paginationId = paginationDiv.id
         const totalPages = Math.floor((sounds.length + 20 - 1) / 20);
+        updatePagination(paginationId, page, totalPages, (p) => {
+            loadFavourites(userID, containerID, p);
+        });
     }).catch(error => {
         console.error("Error:", error);
     });
 }
 
 window.loadFavourites = loadFavourites;
+
+function updatePagination(divID, currentPage, totalPages, onPageClick) {
+    const pagination = document.getElementById(divID);
+    pagination.innerHTML = '';
+
+    if (currentPage > 1) {
+        const firstLink = document.createElement('a');
+        firstLink.textContent = 'First';
+        firstLink.onclick = () => onPageClick(1);
+        pagination.appendChild(firstLink);
+
+        const prevLink = document.createElement('a');
+        prevLink.textContent = 'Before';
+        prevLink.onclick = () => onPageClick(currentPage - 1);
+        pagination.appendChild(prevLink);
+    }
+
+    for (let i = 1; i <= totalPages; i++) {
+        const pageLink = document.createElement('a');
+        pageLink.textContent = i;
+        pageLink.onclick = () => onPageClick(i);
+        if (i === currentPage) {
+            pageLink.classList.add('active');
+        }
+        pagination.appendChild(pageLink);
+    }
+
+    if (currentPage < totalPages) {
+        const nextLink = document.createElement('a');
+        nextLink.textContent = 'Next';
+        nextLink.onclick = () => onPageClick(currentPage + 1);
+        pagination.appendChild(nextLink);
+
+        const lastLink = document.createElement('a');
+        lastLink.textContent = 'Last';
+        lastLink.onclick = () => onPageClick(totalPages);
+        pagination.appendChild(lastLink);
+    }
+}
